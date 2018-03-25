d1db94445246f4946b75390b58c45be554479975
vagrant <becca@themosses.com>
2018-03-25 17:04:53 +0000

    Split Square coding challenge completed

---
+++ split-square/add.py
@@ -0,0 +1,73 @@
+"""Produce new square adding two inputs squares.
+
+Two simple squares can be added::
+
+    >>> s1 = 0
+    >>> s2 = 1
+
+    >>> add(s1, s2)
+    1
+
+A simple square and a split square can be added::
+
+    >>> s1 = 0
+    >>> s2 = [1, 0, 1, 0]
+
+    >>> add(s1, s2)
+    [1, 0, 1, 0]
+
+Two split squares can be added::
+
+    >>> s1 = [0, 0, 0, 1]
+    >>> s2 = [0, 1, 0, 1]
+
+    >>> add(s1, s2)
+    [0, 1, 0, 1]
+
+Nested squares can be added::
+
+    >>> s1 = [0, [1, 1, 1, [0, 0, 0, 0]], [0, 0, 0, 0], 1]
+    >>> s2 = [1, [1, 0, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> add(s1, s2)
+    [1, [1, 1, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+Unevenly-nested squares can be added::
+
+    >>> s1 = [0, [1, 1, 1, 0           ], [0, 0, 0, 0], 1]
+    >>> s2 = [1, [1, 0, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> add(s1, s2)
+    [1, [1, 1, 1, [0, 0, 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> s1 = [0, [1, 1, 1, 1                      ], [0, 0, 0, 0], 1]
+    >>> s2 = [1, [1, 0, 1, [0, [0, 0, 0, 0], 1, 1]], [1, 0, 1, 0], 1]
+
+    >>> add(s1, s2)
+    [1, [1, 1, 1, [1, [1, 1, 1, 1], 1, 1]], [1, 0, 1, 0], 1]
+"""
+
+sums = []
+def add(s1, s2):
+    """Produce new split square adding two input squares."""
+    # import pdb; pdb.set_trace()
+    if type(s1) == type(s2) == int:
+        return (s1 | s2)
+
+    if type(s1) is int:
+        s1 = [s1] * 4
+    if type(s2) is int:
+        s2 = [s2] * 4
+
+    result = []
+    for item1, item2 in zip(s1, s2):
+        result.append(add(item1, item2))
+    return result
+
+    # return [add(item1, item2) for item1, item2 in zip(s1, s2)]
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASS; YOU'RE A RECURSION WIZARD!\n"
